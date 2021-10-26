1ed65658a28c8b4879b68e590297d5333f5ca860
Shivanshudeveloper <developer.shivanshu@gmail.com>
2021-10-26 08:43:34 +0530

    S

---
+++ server/config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://adminuser:admin1234@cluster0.gjuaw.mongodb.net/contentsaas?retryWrites=true&w=majority'
+}
+
+// module.exports = {
+//     MongoURI: 'mongodb://localhost:27017/search'
+// }
